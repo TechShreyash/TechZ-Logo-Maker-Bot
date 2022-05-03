dfd317035bd542db9713bb9b7f6b761ebf759353
shreyashdheemar123@gmail.com <shreyashdheemar123@gmail.com>
2022-05-03 22:22:17 +0530

    Update Logo Maker Bot

--- main/logo.py
+++ main/logo.py
@@ -1,18 +1,22 @@
 from main import LOGO_API_URL1, LOGO_API_URL2
 from typing import Optional
+import aiohttp
 
-async def generate_logo(text: str, square: Optional[bool] = None ):
+async def generate_logo(text: str, square: Optional[bool] = False ):
   "To Create Logos. text = What you want to write on the logo. square = If You Want Square Logo Or Not. Returns Telgraph Image Url"
   
-  square = str(square).capitalize()
+  try:
+    square = str(square).capitalize()
   
-  if square == "True":
-    url = LOGO_API_URL2 + text
-  else:
-    url = LOGO_API_URL1 + text
+    if square == "True":
+      url = LOGO_API_URL2 + text
+    else:
+      url = LOGO_API_URL1 + text
   
-  async with aiohttp.ClientSession() as session: 
-    async with session.get(url) as resp:  
-      img_url = resp.url
+    async with aiohttp.ClientSession() as session: 
+      async with session.get(url) as resp:  
+        img_url = resp.url
+    except Exception as e:
+      return "error" + str(e)
       
   return str(img_url)
