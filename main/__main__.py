23de355c671b865b882832ad71c75a8521376200
shreyashdheemar123@gmail.com <shreyashdheemar123@gmail.com>
2022-05-04 10:13:43 +0530

    Added Image And Repo Button In Start Message

--- main/__main__.py
+++ main/__main__.py
@@ -21,14 +21,15 @@ HELP = """
 `/logo TechZBots`
 `/logosq TechZBots`
 """
+
 # Commands
 @app.on_message(filters.command("start"))
 async def start(bot, message):
-  await message.reply_text(START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu")]]))
+  await message.reply_photo("https://telegra.ph/file/7a98ead33e7b99fd82cc7.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu"), InlineKeyboardButton(text="Repo", url="https://github.com/TechShreyash/TechZ-Logo-Maker-Bot")]]))
 
 @app.on_message(filters.command("help"))
 async def help(bot, message):
-  await message.reply_text(HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
+  await message.reply_photo("https://telegra.ph/file/7a98ead33e7b99fd82cc7.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
 
 @app.on_message(filters.command("logo") & filters.incoming & filters.text & ~filters.forwarded & (
   filters.group | filters.private))
@@ -100,7 +101,7 @@ async def logo(bot, message):
 @app.on_callback_query(filters.regex("start_menu"))
 async def start_menu(_,query):
   await query.answer()
-  await query.message.edit(START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu")]]))
+  await query.message.edit(START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu"),InlineKeyboardButton(text="Repo", url="https://github.com/TechShreyash/TechZ-Logo-Maker-Bot")]]))
 
 @app.on_callback_query(filters.regex("help_menu"))
 async def help_menu(_,query):
