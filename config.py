5efe87c79824719d33d9a3de9f2d6ac1c18132da
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-02-27 20:16:50 +0530

    LogoMakerBot Fixed

--- config.py
+++ config.py
@@ -1,16 +1,45 @@
+import sys
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = ""
+API_HASH = ""
 
-## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+# Get it from @Botfather in Telegram.
+BOT_TOKEN = ""
 
-# API By TechZBots || https://t.me/TechZBots
-LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
+# Create Your Own ApiKey From @TechZApiBot To Use Logo Api
+API_KEY = ""
 
-LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
+
+# Don't Change Anything Below This Line
+
+
+if not API_ID or API_ID.strip() == "":
+    API_ID = int(getenv("API_ID"))
+if not API_HASH or API_HASH.strip() == "":
+    API_HASH = getenv("API_HASH")
+if not BOT_TOKEN or BOT_TOKEN.strip() == "":
+    BOT_TOKEN = getenv("BOT_TOKEN")
+if not API_KEY or API_KEY.strip() == "":
+    API_KEY = getenv("API_KEY")
+
+
+LOGO_API_URL1 = f"https://techzapi.azurewebsites.net/logo?api_key={API_KEY}&text="
+
+LOGO_API_URL2 = (
+    f"https://techzapi.azurewebsites.net//logo?api_key={API_KEY}&square=True&text="
+)
+
+
+if not API_KEY:
+    print("Error: API_KEY Not Found!")
+    print("Please Get Your Own ApiKey From @TechZApiBot To Use Logo Api")
+    sys.exit()
+elif API_KEY.strip() == "":
+    print("Error: API_KEY Not Found!")
+    print("Please Get Your Own ApiKey From @TechZApiBot To Use Logo Api")
+    sys.exit()
--- main/logo.py
+++ main/logo.py
@@ -1,21 +1,26 @@
 from main import LOGO_API_URL1, LOGO_API_URL2, session
 from typing import Optional
 import aiohttp
+from string import ascii_letters, digits
+import random
 
-async def generate_logo(text: str, square: Optional[bool] = False ):
-  "To Create Logos. text = What you want to write on the logo. square = If You Want Square Logo Or Not. Returns Telgraph Image Url"
-  
-  try:
-    square = str(square).capitalize()
-  
-    if square == "True":
-      url = LOGO_API_URL2 + text
-    else:
-      url = LOGO_API_URL1 + text
-  
-    resp = await session.get(url)  
-    img_url = resp.url
-  except Exception as e:
-    return "error" + str(e)
-      
-  return str(img_url)
+
+async def generate_logo(text: str, square: Optional[bool] = False):
+    "To Create Logos. text = What you want to write on the logo. square = If You Want Square Logo Or Not. Returns Telgraph Image Url"
+
+    try:
+        square = str(square).capitalize()
+
+        if square == "True":
+            url = LOGO_API_URL2 + text
+        else:
+            url = LOGO_API_URL1 + text
+
+        resp = await session.get(url)
+        img = "".join(random.choices(ascii_letters + digits, k=10)) + ".jpg"
+        with open(img, "wb") as f:
+            f.write(await resp.read())
+    except Exception as e:
+        print("error" + str(e))
+        return "error" + str(e)
+    return img
